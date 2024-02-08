7c442e7c04f9a76d73c491a63f4183c0e089e55c
Alberto Lima <alberto.btlima@outlook.com>
2024-02-08 16:31:47 -0300

    Update script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const searchInput = document.getElementById("search-input");
 function requestApi(searchTerm) {
   fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
     .then((response) => response.json())
-    .then((results) => displayResults(results));
+    .then((results) => displayResults(results))
 }
 
 function displayResults(results) {
@@ -33,49 +33,3 @@ searchInput.addEventListener("input", function () {
   }
   requestApi(searchTerm);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-/* const resultArtist = document.getElementById("result-artist");
-const resultPlaylist = document.getElementById('result-playlists');
-const searchInput = document.getElementById('search-input');
-
-function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
-}
-
-function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistImage = document.getElementById('artist-img');
-    const artistName = document.getElementById('artist-name');
-
-    result.forEach((element) => {
-        artistImage.src = element.urlImg;
-        artistName.innerText = element.name;
-    });
-
-    resultArtist.classList.remove('hidden');
-}
-
-document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
-    }
-    
-    requestApi(searchTerm);
-}) */
