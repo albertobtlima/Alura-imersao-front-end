298effd588c76e889dfb70e29fc7bec7d6ab1783
Alberto Lima <alberto.btlima@outlook.com>
2024-02-07 21:22:00 -0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
+const playlistContainer = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
+
+function requestApi(searchTerm) {
+  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    .then((response) => response.json())
+    .then((results) => displayResults(results));
+}
+
+function displayResults(results) {
+  hidePlaylists();
+  const artistImage = document.getElementById("artist-img");
+  const artistName = document.getElementById("artist-name");
+
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+  });
+  resultArtist.classList.remove("hidden");
+}
+
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
+
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultArtist.classList.add("hidden");
+    playlistContainer.classList.remove("hidden");
+    return;
+  }
+  requestApi(searchTerm);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/* const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
+const searchInput = document.getElementById('search-input');
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
@@ -11,12 +58,12 @@ function requestApi(searchTerm) {
 
 function displayResults(result) {
     resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
+    const artistName = document.getElementById('artist-name');
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
+    result.forEach((element) => {
         artistImage.src = element.urlImg;
+        artistName.innerText = element.name;
     });
 
     resultArtist.classList.remove('hidden');
@@ -31,4 +78,4 @@ document.addEventListener('input', function () {
     }
     
     requestApi(searchTerm);
-})
+}) */
